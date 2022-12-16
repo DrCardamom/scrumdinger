9287edb4c3405dbaf41944d438b4422609e8d3c6
DrCardamom <erika.shimba@400f.jp>
2022-12-16 14:52:04 +0900

    Updating app data

---
+++ Scrumdinger/Models/History.swift
@@ -0,0 +1,22 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Erika Shimba on 2022/12/16.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    var lengthInMinutes: Int
+    
+    init(id: ID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+    }
+}
